7c6679569762da4b23f289299571ec73a1a5318f
Salem Ait Ami <salemaitami@uvic.ca>
2024-12-13 15:51:03 -0800

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output : "export",
+  
+  output: "export",
+
+  basePath: "/nextjs-github-pages",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
