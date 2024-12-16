764856e9ca5856ea409542e3b2325f308429bbd1
Salem Ait Ami <salemaitami@uvic.ca>
2024-12-15 20:20:29 -0800

    Removed unused fonts, Added webpack for svg

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,24 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
+};
+
+module.exports = {
+  webpack(config : NextConfig) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+       // for webpack 5 use
+       // { and: [/\.(js|ts)x?$/] }
+      },
+      
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
